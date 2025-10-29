6564e163b8771e9dbe00e0cda25e2321f9c10189
fatima-Basheer <fatimabasheer8214@gmail.com>
2025-10-28 22:18:30 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import ControlledComp from "./ControlledComp";
+import GetInputVal from "./GetInputVal";
+
+function App() {
+
+  return (
+    <>
+      {/* <GetInputVal></GetInputVal> */}
+      <ControlledComp></ControlledComp>
+    </>
+  );
+}
+
+export default App;
